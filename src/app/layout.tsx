d7426a491f44385c1372c5741ae3cef988f17da4
Isaiah Nazaire <isaiahjazz01@gmail.com>
2025-05-06 17:12:07 -0400

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" style={{ scrollBehavior: "smooth" }}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
